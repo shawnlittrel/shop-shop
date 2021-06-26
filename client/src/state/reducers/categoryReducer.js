9c9ac919e7f0b08235d6ba44bba634c17a3a7441
Shawn Littrel <shawnlittrel@protonmail.com>
2021-06-25 22:54:30 -0700

    finish up refactor

--- client/src/state/reducers/categoryReducer.js
+++ client/src/state/reducers/categoryReducer.js
@@ -4,11 +4,13 @@ export default function categoryReducer(state = initialState, action) {
      switch (action.type) {
           case 'UPDATE_CATEGORIES': {
                return {
+                    ...state,
                     categories: [...action.categories]
                };
           }
           case 'UPDATE_CURRENT_CATEGORY': {
                return {
+                    ...state,
                     currentCategory: action.currentCategory
                };
           }
--- client/src/state/store.js
+++ client/src/state/store.js
@@ -1,12 +1,11 @@
-import { createStore } from 'redux';
+import { createStore } from "redux";
 //import reducers
-import { combineReducers } from "redux";
-import cartReducer from "./reducers/cartReducer";
-import productReducer from "./reducers/productReducer";
-import categoryReducer from "./reducers/categoryReducer";
-import currentCategoryReducer from './reducers/currentCategoryReducer';
-import cartOpenReducer from './reducers/cartOpenReducer';
-
+// import { combineReducers } from "redux";
+// import cartReducer from "./reducers/cartReducer";
+// import productReducer from "./reducers/productReducer";
+// import categoryReducer from "./reducers/categoryReducer";
+// import currentCategoryReducer from "./reducers/currentCategoryReducer";
+// import cartOpenReducer from "./reducers/cartOpenReducer";
 
 const preloadedState = {
   cart: [],
@@ -16,20 +15,75 @@ const preloadedState = {
   products: [],
 };
 
-const rootReducer = combineReducers({
-  //each reducer only sees and manages its own chunk of the whole global state
-  //value of state.cart is whatever cartReducer returns
-  cart: cartReducer,
-  //state.cartOpen comes from cartOpenReducer
-  cartOpen: cartOpenReducer,
-  //value of state.product is whatever productReducer returns
-  products: productReducer,
-  //value of state.category is whatever categoryReducer returns
-  categories: categoryReducer,
-  //state.currentCategory comes from currentCategoryReducer
-  currentCategory: currentCategoryReducer
-});
+function rootReducer(state = preloadedState, action) {
+  switch (action.type) {
+    case "TOGGLE_CART":
+      return {
+        cartOpen: !state.cartOpen,
+      };
+    case "ADD_TO_CART":
+      return {
+        ...state,
+        cartOpen: true,
+        cart: [...state.cart, action.product],
+      };
+    case "ADD_MULTIPLE_TO_CART":
+      return {
+        ...state,
+        cart: [state.cart, ...action.products],
+      };
+    case "UPDATE_CART_QUANTITY":
+      return {
+        ...state,
+        cartOpen: true,
+        cart: state.cart.map((product) => {
+          if (action._id === product._id) {
+            product.purchaseQuantity = action.purchaseQuantity;
+          }
+          return product;
+        }),
+      };
+    case "REMOVE_FROM_CART":
+      let newState = state.cart.filter((product) => {
+        return product._id !== action._id;
+      });
 
+      return {
+        ...state,
+        cartOpen: newState.length > 0,
+        cart: newState,
+      };
+    case "CLEAR_CART":
+      return {
+        ...state,
+        cartOpen: false,
+        cart: [],
+      };
+    case "UPDATE_CATEGORIES": {
+      return {
+        ...state,
+        categories: [...action.categories],
+      };
+    }
+    case "UPDATE_CURRENT_CATEGORY": {
+      return {
+        ...state,
+        currentCategory: action.currentCategory,
+      };
+    }
+
+    case "UPDATE_PRODUCTS": {
+      return {
+        ...state,
+        products: [...action.products],
+      };
+    }
+
+    default: {
+      return state;
+    }
+  }
+}
 
 const store = createStore(rootReducer, preloadedState);
 
