956c42fcbdb63fb428207682973ceb94aeac5bf7
Shawn Littrel <shawnlittrel@protonmail.com>
2021-06-25 22:37:18 -0700

    working on initial state

--- client/src/state/reducers/categoryReducer.js
+++ client/src/state/reducers/categoryReducer.js
@@ -4,13 +4,11 @@ export default function categoryReducer(state = initialState, action) {
      switch (action.type) {
           case 'UPDATE_CATEGORIES': {
                return {
-                    ...state,
                     categories: [...action.categories]
                };
           }
           case 'UPDATE_CURRENT_CATEGORY': {
                return {
-                    ...state,
                     currentCategory: action.currentCategory
                };
           }
